8ca25c66d73edd3f12b38ef93330f6708cdb5095
Rafael <rafael.pierre@gmail.com>
2024-07-07 16:18:54 +0200

    mlflow/hf

--- viper/core/scanners.py
+++ viper/core/scanners.py
@@ -10,8 +10,8 @@ class EnvironmentScanner:
     parquet_file: str = PARQUET_FILE
 
     def __init__(self, **kwargs):
-        super().__init__(kwargs)
-        # Initialize a Spark session
+
+        # Initialize a Spark session - TODO: centralize in a Singleton class
         self.spark = SparkSession.builder \
             .appName("Read JSON Files") \
             .config("spark.sql.shuffle.partitions", "200") \
@@ -20,4 +20,17 @@ class EnvironmentScanner:
             .getOrCreate()
 
     def scan(self):
+        raise NotImplementedError()
+    
+@dataclass
+class MlFlowScanner:
+
+    def __init__(self, **kwargs):
+        raise NotImplementedError()
+    
+
+@dataclass
+class HuggingFaceScanner:
+
+    def __init__(self, **kwargs):
         raise NotImplementedError()
